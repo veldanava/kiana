a23faf916aa1089e3122855c759a64262f202c2d
zeta ゼータ <maisan098704@gmail.com>
2023-03-07 19:40:09 +0700

    Update j.js

--- j.js
+++ j.js
@@ -1,5 +1,28 @@
-$('span').on('mouseenter', '.uwu', function(){
-    $('.uwu').html(Math.random().toString(36).substring(7));
-}).on('mouseleave', function(){
-    $('.uwu').html('Kaslana.');
-});
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector(".uwu").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
